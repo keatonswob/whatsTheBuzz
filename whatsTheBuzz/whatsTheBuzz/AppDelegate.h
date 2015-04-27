ee006b12b628e13dbfaf568060aee45c65291914
Keaton Swoboda <keaton.swoboda@gmail.com>
2015-04-27 11:55:20 -0400

    add project

---
+++ whatsTheBuzz/whatsTheBuzz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  whatsTheBuzz
+//
+//  Created by Keaton Swoboda on 4/27/15.
+//  Copyright (c) 2015 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ whatsTheBuzz/whatsTheBuzz/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  whatsTheBuzz
+//
+//  Created by Keaton Swoboda on 4/27/15.
+//  Copyright (c) 2015 The Iron Yard. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
